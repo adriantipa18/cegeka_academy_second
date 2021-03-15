efa1484f51e5c00cd950838ddb3824a7a93fe23a
Adrian <Adrian@DESKTOP-JE8H084>
2021-03-16 00:02:33 +0200

    cloned homework

---
+++ GenericsExercise.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsExercise.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            // your code here
+        }
+    }
+}
